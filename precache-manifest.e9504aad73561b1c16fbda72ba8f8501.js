b1a66bd78907e62463031f6cc255405fb9d81e76
rizwanishaq <rizwanishaq@gmail.com>
2020-01-02 15:49:00 +0100

    Updates

---
+++ precache-manifest.e9504aad73561b1c16fbda72ba8f8501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9124eebab6d9959d6c979ce09ee6bf",
+    "url": "/lightsout/index.html"
+  },
+  {
+    "revision": "de22630fac6643c808b8",
+    "url": "/lightsout/static/css/main.6d515b10.chunk.css"
+  },
+  {
+    "revision": "22a251ecdceace71d94d",
+    "url": "/lightsout/static/js/2.29e5c693.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/lightsout/static/js/2.29e5c693.chunk.js.LICENSE"
+  },
+  {
+    "revision": "de22630fac6643c808b8",
+    "url": "/lightsout/static/js/main.8e398271.chunk.js"
+  },
+  {
+    "revision": "51a4794f3f9aceb5384b",
+    "url": "/lightsout/static/js/runtime-main.ecd8ed91.js"
+  }
+]);
